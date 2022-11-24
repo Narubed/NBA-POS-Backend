6963d184984acae21174780b6bf2cb17f1fc2224
Narubed Kaewserd <40911777+Narubed@users.noreply.github.com>
2022-11-24 13:28:02 +0700

    update checkHeaders

--- index.js
+++ index.js
@@ -5,51 +5,60 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const checkToken = require("./lib/checkToken");
 const connection = require("./config/db");
+const authHeader = require("./lib/checkHeaders");
+
 connection();
 
 // middlewares
 
 const skipTheCheckingOfOrigin = false;
 
-const allowedOrigins = [
-  "http://192.168.1.59:3010",
-  "http://192.168.1.97:3010",
-  "http://192.168.1.11:3010",
-  "http://192.168.1.2:3010",
-  "http://localhost:3010",
-  "https://nba-pos.nbadigitalservice.com",
-];
-app.use(express.json());
-app.use("/api/nba-pos/signin", require("./routes/signin")); // login ไม่เช็ค Origin
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      console.log(origin);
-      // allow requests with no origin (like mobile apps or curl requests)
-      // or allow all origines (skipTheCheckingOfOrigin === true)
-      if (!origin || skipTheCheckingOfOrigin === true)
-        return callback(new Error(msg), true);
+// const allowedOrigins = [
+//   "http://192.168.1.59:3010",
+//   "http://192.168.1.97:3010",
+//   "http://192.168.1.11:3010",
+//   "http://192.168.1.2:3010",
+//   "http://localhost:3010",
+//   "https://nba-pos.nbadigitalservice.com",
+// ];
+// app.use(express.json());
+// app.use("/api/nba-pos/signin", require("./routes/signin")); // login ไม่เช็ค Origin
 
-      // -1 means that the user's origin is not in the array allowedOrigins
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
+// app.use(
+//   cors({
+//     origin: "http://192.168.1.59:3001",
+//   })
+// );
 
-        return callback(new Error(msg), false);
-      }
-      // origin is in the array allowedOrigins so authorization is granted
-      return callback(null, true);
-    },
-  })
-);
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       console.log(origin);
+//       // allow requests with no origin (like mobile apps or curl requests)
+//       // or allow all origines (skipTheCheckingOfOrigin === true)
+//       if (!origin || skipTheCheckingOfOrigin === true)
+//         return callback(new Error(msg), true);
+
+//       // -1 means that the user's origin is not in the array allowedOrigins
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         var msg =
+//           "The CORS policy for this site does not " +
+//           "allow access from the specified Origin.";
+
+//         return callback(new Error(msg), false);
+//       }
+//       // origin is in the array allowedOrigins so authorization is granted
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 // LOGIN
 
 app.use("/api/nba-pos/delete_image", require("./routes/deleteImage"));
 // routes
-app.use("/api/nba-pos/owners", require("./routes/owner"));
-app.use("/api/nba-pos/branch", require("./routes/branch"));
+app.use("/api/nba-pos/owners", authHeader, require("./routes/owner"));
+app.use("/api/nba-pos/branch", authHeader, require("./routes/branch"));
 app.use("/api/nba-pos/products", require("./routes/product"));
 app.use("/api/nba-pos/products/history", require("./routes/product.history"));
 app.use("/api/nba-pos/employee", require("./routes/employee"));
